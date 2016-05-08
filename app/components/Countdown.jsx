19867f099c82b5aecf0511aac42f463396866bb5
Miyano <dr000w@gmail.com>
2016-05-08 17:33:42 -0400

    Created Clock component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,10 +1,11 @@
 import React, {Component } from 'react';
+import Clock from 'Clock';
 
 export class Countdown extends Component {
   render() {
     return (
       <div>
-        Countdown.jsx
+        <Clock totalSeconds={129}/>
       </div>
     )
   }
